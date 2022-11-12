3cc6e79bd839b1969300eb864ae009f11f189fe8
Roman Geathers <romangeathersdev@gmail.com>
2022-11-12 12:41:52 -0500

    Finish part 3 A exercises

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'uknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+app.use(requestLogger)
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 const PORT = 3001
 app.listen(PORT, () => {
@@ -31,4 +49,58 @@ let persons = [
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const requestID = Number(request.params.id)
+  const person = persons.find(person => person.id === requestID)
+
+  if (person === undefined) {
+    return response.status(404).end()
+  }
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const requestID = Number(request.params.id)
+  persons = persons.filter(person => person.id !== requestID)
+  response.status(204).end()
+})
+
+const checkExistingPerson = (body) => {
+  const exists = persons.find(person => body.name === person.name)
+  return exists
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(request.body)
+
+  if (checkExistingPerson(body)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  } else if (body.name === '' || body.number === '') {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const newPerson = {
+    id: Math.ceil(Math.random() * 100),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(newPerson)
+
+  response.json(newPerson)
 })
